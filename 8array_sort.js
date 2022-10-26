6c537acc2697f7162cb5f77f77dcdb31ead6240c
Rehan Ahmad <rehanmakhdoomi93@gmail.com>
2022-10-26 20:25:48 +0530

    array

--- 8array_sort.js
+++ 8array_sort.js
@@ -1,16 +1,21 @@
 // Array sorting  with loop
 
 let data = [40, 30, 12, 25, 7, 90567, 99, 66, 34, 2, 99];
-// for (let i = 0; i < data.length; i++) {
-//   for (let j = 0; j < data.length; j++) {
-//     if (data[j] > data[j + 1]) {
-//       let temp = data[j];
-//       data[j] = data[j + 1];
-//       data[j + 1] = temp;
-//     }
-//   }
-// }
-const res = data.sort((a, b) => {
-  return b - a;
-});
-console.log(res);
+for (let i = 0; i < data.length; i++) {
+  for (let j = 0; j < data.length; j++) {
+    if (data[j] > data[j + 1]) {
+      let temp = data[j];
+      data[j] = data[j + 1];
+      data[j + 1] = temp;
+    }
+  }
+}
+
+console.log(data.reverse());
+
+// use bydefault method for sorting
+
+// const res = data.sort((a, b) => {
+//   return b - a;
+// });
+// console.log(res);
