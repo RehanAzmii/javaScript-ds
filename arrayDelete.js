6d607a4d86f13a98da856d25a8c1a3d5974b9e2f
Rehan Ahmad <rehanmakhdoomi93@gmail.com>
2022-11-07 11:03:44 +0530

    find fisrt latter of paragraph

--- arrayDelete.js
+++ arrayDelete.js
@@ -6,13 +6,36 @@
 // data.length = data.length - 1;
 // console.log(data);
 
-let btn = document.getElementById("button").addEventListener("click", () => {
-  let data = [20, 30, 45, 76, 20, 80];
-  let position = document.getElementById("position").value;
-  position = parseInt(position);
-  for (let i = position; i < data.length - 1; i++) {
-    data[i] = data[i + 1];
-  }
-  data.length = data.length - 1;
-  console.log(data);
-});
+// let btn = document.getElementById("button").addEventListener("click", () => {
+//   let data = [20, 30, 45, 76, 20, 80];
+//   let position = document.getElementById("position").value;
+//   position = parseInt(position);
+//   for (let i = position; i < data.length - 1; i++) {
+//     data[i] = data[i + 1];
+//   }
+//   data.length = data.length - 1;
+//   console.log(data);
+// });
+const getFirstLeter = (str) => {
+  const firstLaters = str
+    .split(" ")
+    .map((word) => word[0])
+    .join("");
+  return firstLaters;
+};
+console.log(getFirstLeter("rehan Ahmad khan rehan"));
+
+function getFirstLetters(str) {
+  const firstLetters = str
+    .split(" ")
+    .map((word) => word[0])
+    .join("");
+
+  return firstLetters;
+}
+
+// 👇️ ABC
+console.log(getFirstLetters("Alice, Bob, Charlie"));
+
+// 👇️ ONE
+console.log(getFirstLetters("Oscar   Noah   Emily."));
